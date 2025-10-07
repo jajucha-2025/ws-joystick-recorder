bd14b66fb729827d532cc4b7d268d238321bfbcc
KGH1113 <gangguhyeon1113@gmail.com>
2025-10-07 16:23:08 +0900

    feat: rk2로 자주차 위치 구하기

--- app/control.py
+++ app/control.py
@@ -1,11 +1,16 @@
-def on_joystick(x: float, y: float):
+from .state import get_pos, set_car, _car
+
+def on_joystick(x: float, y: float, dt: float):
+  print(dt)
+  max_speed = 15
+  dSteer = round(x * 10)
+  dSpeed = round(y * max_speed)
+
+  pos = get_pos(_car["wheelbase"], dt, _car["pos"], dSteer*2, dSpeed)
+  set_car(_car["wheelbase"], pos)
+
   try:
     import jchm
-
-    max_speed = 15
-    dSteer = round(x * 10)
-    dSpeed = round(y * max_speed)
-
     jchm.control.set_motor(dSteer, dSteer, dSpeed)
-  except ModuleNotFoundError:
+  except:
     pass
